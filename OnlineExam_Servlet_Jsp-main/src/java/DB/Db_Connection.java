d8108f0267dba6ca686063e17ba7f485cfc3def5
laxmikanth-k <132427736+laxmikanth-k@users.noreply.github.com>
2025-11-08 02:09:58 +0530

    Update Db_Connection.java

--- OnlineExam_Servlet_Jsp-main/src/java/DB/Db_Connection.java
+++ OnlineExam_Servlet_Jsp-main/src/java/DB/Db_Connection.java
@@ -14,10 +14,8 @@ public class Db_Connection {
     public Db_Connection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            //String ConDb = "jdbc:mysql://localhost:3306/onlineexams";
             String ConDb = "jdbc:mysql://node1160-env-2944662.sjc3.cloudsig.ma/onlineexams";
             String ConUsername = "root";
-            //String ConPassword = "Khan@786";
             String ConPassword = "AEAgmf14107";
             Con = DriverManager.getConnection(ConDb, ConUsername, ConPassword);
         } catch (Exception e) {
